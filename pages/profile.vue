d8d66c46055cfa24fb02675c996bff2407094900
Marek Sklář <99105387+MarekSklar@users.noreply.github.com>
2024-05-20 13:46:32 +0200

    Update profile.vue

--- pages/profile.vue
+++ pages/profile.vue
@@ -15,10 +15,19 @@ const pfpFormat = pfpPath.split('.').pop();
 </script>
 
 <template>
-    <main v-if="sessionToken">
-        <img :src="'data:image/' + pfpFormat + ';base64,' + pfp" width="256" height="256" class="m-1.5"/>
-
-        Name: {{ profile.name }} <br>
-        E-Mail: {{ profile.email }}<br>        
-    </main>    
+    <div v-if="sessionToken && sessionToken !== 'null'">
+        <div>
+            <img :src="'data:image/' + pfpFormat + ';base64,' + pfp" width="256" height="256" class="size-64 rounded-full object-cover"/>
+            <h2 class="text-3xl text-gray-800">{{ profile.name }}</h2>
+            <p>E-Mail: {{ profile.email }}</p>
+        </div>
+    </div>
+    <div v-else>
+        <div>
+            <h2>You are not logged in.</h2>
+            <div class="p-4 bg-gray-500 text-white">
+                <NuxtLink to="/login">Login</NuxtLink>
+            </div>
+        </div>
+    </div>
 </template>
