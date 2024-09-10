37ccb5794e2ea650d0673d440b5101ce5323d649
Marek Sklář <99105387+MarekSklar@users.noreply.github.com>
2024-09-10 13:08:59 +0200

    Update invalidSession.vue

--- pages/invalidSession.vue
+++ pages/invalidSession.vue
@@ -4,12 +4,14 @@
 
 <template>
   <div class="flex items-center justify-center gap-6 w-screen h-screen bg-gray-50 dark:bg-gray-900">
+    <!-- login redirect -->
     <div class="flex flex-col items-center gap-8 px-20 py-12 rounded-xl shadow-lg bg-white dark:bg-gray-800 dark:shadow-md">
       <h2>Already have an account?</h2>
       <NuxtLink to="/login">
         <button class="px-6 py-3 rounded-md bg-green-200">Login</button>
       </NuxtLink>
     </div>
+    <!-- register redirect -->
     <div class="flex flex-col items-center gap-8 px-20 py-12 rounded-xl shadow-lg bg-white dark:bg-gray-800 dark:shadow-md">
       <h2>Don't have an account?</h2>
       <NuxtLink to="/register">
