99cd7c5e881fe2d622451ad0d75481cb86f8d05f
Marek Sklář <99105387+MarekSklar@users.noreply.github.com>
2024-09-16 13:13:14 +0200

    Design task context menu
    + dark mode users, forgot in last commit

--- components/calendar/taskContextMenu.vue
+++ components/calendar/taskContextMenu.vue
@@ -9,10 +9,27 @@
 </script>
 
 <template>
-  <div class="fixed z-50 bg-white" :style="{ left: props.x + 'px', top: props.y + 'px' }">
-    <ul>
-      <button @click="emit('onDuplicateTask')">Duplicate</button><br>
-      <button @click="emit('onDeleteTask')">Delete</button>
-    </ul>
+  <div class="background fixed z-30 min-w-36 px-3 py-3 rounded-md shadow-md" :style="{ left: props.x + 'px', top: props.y + 'px' }">
+    <h3 class="text-sm text-gray-800 mb-3 px-2">Task quick edit</h3>
+    <div class="flex flex-col text-sm font-semibold">
+    <button @click="emit('onDuplicateTask')" class="line">
+      <SvgDuplicate />
+      Duplicate
+    </button>
+    <button @click="emit('onDeleteTask')" class="line text-red-500 hover:!bg-red-50">
+      <SvgDelete class="fill-red-500" />
+      Delete
+    </button>
+    </div>
   </div>
 </template>
+
+<style scoped>
+svg {
+  @apply size-4;
+}
+
+.line {
+  @apply flex items-center gap-2 px-2 py-1 rounded-md hover:bg-gray-100;
+}
+</style>
--- pages/users.vue
+++ pages/users.vue
@@ -37,20 +37,20 @@ function toggleEdit() {
 <template>
     <UsersEdit v-if="showEdit" :editedProfile="editedProfile" :profiles="profiles" @toggleEdit="toggleEdit" />
     <div class="simpleCardBox py-10">
-        <div class="card gap-4 w-5/6 max-h-[100%] h-fit px-6 py-6">
+        <div class="card background gap-4 w-5/6 max-h-[100%] h-fit px-6 py-6">
             <div class="flex w-full px-3">
-                <button @click="addNewProfile()" class="flex gap-2 px-4 py-2 font-semibold bg-gray-100 rounded-md">
-                    <SvgAddPerson />
+                <button @click="addNewProfile()" class="flex gap-2 px-4 py-2 font-semibold bg-gray-100 dark:bg-dark-600 rounded-md">
+                    <SvgAddPerson class="dark:fill-dark-50" />
                     <p>Add new user</p>
                 </button>
             </div>
-            <div class="card gap-0 size-full p-3 shadow-none overflow-auto overflow-x-hidden">
-                <div v-for="profile in profiles" @click="editProfile(profile)" class="flex items-center gap-3 w-full px-4 py-2 rounded-md odd:bg-gray-100 cursor-pointer">
+            <div class="card background gap-0 size-full p-3 shadow-none dark:shadow-none overflow-auto overflow-x-hidden">
+                <div v-for="profile in profiles" @click="editProfile(profile)" class="flex items-center gap-3 w-full px-4 py-2 rounded-md odd:bg-gray-100 dark:odd:bg-dark-600 cursor-pointer">
                     <img v-if="profileImages![profile.uuid]" :src="'data:image/jpg;base64,' + profileImages![profile.uuid]" class="size-7 rounded-full object-cover">
                     <div class="flex justify-between w-full">
                         <h3>{{ profile.name }}</h3>
                         <button>
-                            <SvgMore class="size-6" />
+                            <SvgMore class="size-6 dark:fill-dark-50" />
                         </button>
                     </div>
                 </div>
